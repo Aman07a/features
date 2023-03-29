c300d3362967fda04ca84d2be14f474fe64a0a48
Aman07a <aman16.aa17@gmail.com>
2023-03-29 09:49:35 +0200

    Why Typed Arrays?

--- annotations/arrays.js
+++ annotations/arrays.js
@@ -1,3 +1,12 @@
 var carMakers = ['ford', 'toyota', 'chevy'];
 var dates = [new Date(), new Date()];
 var carsByMake = [];
+// Help with inference when extracting values
+var car = carMakers[0];
+var myCar = carMakers.pop();
+// Prevent incompatible values
+// carMakers.push(100);
+// Help with 'map'
+carMakers.map(function (car) {
+    return car.toUpperCase();
+});
