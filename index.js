a1f8ca6be9cc257b9ba26f54e10e715690bbe93b
Aman07a <aman16.aa17@gmail.com>
2023-03-29 08:04:22 +0200

    Annotations Around Functions

---
+++ index.js
@@ -0,0 +1,11 @@
+var today = new Date();
+today.getMonth();
+var Person = {
+    age: 20,
+};
+var Color = /** @class */ (function () {
+    function Color() {
+    }
+    return Color;
+}());
+var red = new Color();
