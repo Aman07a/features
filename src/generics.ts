805c85fcb9858c7b2182ad47b5833d25d7978971
Aman07a <aman16.aa17@gmail.com>
2023-03-31 13:11:28 +0200

    Generic Constraints

--- src/generics.ts
+++ src/generics.ts
@@ -44,3 +44,29 @@ function printAnything<T>(arr: T[]): void {
 }
 
 printAnything(["a", "b", "c"]);
+
+// Generic Constraints
+class Car {
+  print() {
+    console.log("I am a car");
+  }
+}
+
+class House {
+  print() {
+    console.log("I am a house");
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHouseOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+printHouseOrCars([new House(), new House()]);
+printHouseOrCars([new Car(), new Car()]);
